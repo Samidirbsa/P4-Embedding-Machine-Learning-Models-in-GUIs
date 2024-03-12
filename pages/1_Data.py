088765c2b92a8e6504cf453cbad6d61945178b1e
Samuel Dribsa <samuel47dribsa@gmail.com>
2024-03-12 22:46:56 +0300

    add ml_dataset and data.py

--- pages/1_Data.py
+++ pages/1_Data.py
@@ -31,6 +31,33 @@ data = read_csv_from_url(csv_url)
 if data is not None:
     st.title("Data Page")
     st.write("This is the data page.")
+    st.write("Description of Columns:")
+    column_description = {
+        'Gender': 'Whether the customer is a male or a female',
+        'SeniorCitizen': 'Whether a customer is a senior citizen or not',
+        'Partner': 'Whether the customer has a partner or not (Yes, No)',
+        'Dependents': 'Whether the customer has dependents or not (Yes, No)',
+        'Tenure': 'Number of months the customer has stayed with the company',
+        'PhoneService': 'Whether the customer has a phone service or not (Yes, No)',
+        'MultipleLines': 'Whether the customer has multiple lines or not',
+        'InternetService': "Customer's internet service provider (DSL, Fiber Optic, No)",
+        'OnlineSecurity': 'Whether the customer has online security or not (Yes, No, No Internet)',
+        'OnlineBackup': 'Whether the customer has online backup or not (Yes, No, No Internet)',
+        'DeviceProtection': 'Whether the customer has device protection or not (Yes, No, No internet service)',
+        'TechSupport': 'Whether the customer has tech support or not (Yes, No, No internet)',
+        'StreamingTV': 'Whether the customer has streaming TV or not (Yes, No, No internet service)',
+        'StreamingMovies': 'Whether the customer has streaming movies or not (Yes, No, No Internet service)',
+        'Contract': 'The contract term of the customer (Month-to-Month, One year, Two year)',
+        'PaperlessBilling': 'Whether the customer has paperless billing or not (Yes, No)',
+        'PaymentMethod': 'The customer\'s payment method (Electronic check, mailed check, Bank transfer(automatic), Credit card(automatic))',
+        'MonthlyCharges': 'The amount charged to the customer monthly',
+        'TotalCharges': 'The total amount charged to the customer',
+        'Churn': 'Whether the customer churned or not (Yes or No)'
+    }
+    for column, description in column_description.items():
+        st.write(f"- **{column}**: {description}")
+
+    st.write("This dataset contains information about telecommunications customers and their churn behavior.")
 
     @st.cache_data()
     def select_all_features(df):
